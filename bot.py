9d4948e8290d0bacfca37320ed1ba1c6580756a8
Foxfair Hu <foxfair@gmail.com>
2020-07-06 02:19:23 +0000

    Modify README.md for minor wording issues. Add bot.py, the main module for DreamitBot. Add a module of ext/sheet to read/write/change data in a Google spreadsheet via Google Sheet APIs.

---
+++ bot.py
@@ -0,0 +1,183 @@
+import ast
+import datetime
+import inspect
+import json
+import logging
+import os
+
+import discord
+import aiohttp
+import asyncio
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+from ext import auth_config
+from ext import checks
+from ext import utils
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+# GUILD = os.getenv('DISCORD_GUILD')
+BOT_DESC = os.getenv('BOT_DESC')
+INVITE_URL = 'https://discord.com/api/oauth2/authorize?client_id=725436099018883153&permissions=216128&scope=bot'
+
+# Two prefixes for different usages: ? for user, ! for admin
+BOT_PREFIX = {
+        'user': '?',
+        'admin': '!'
+        }
+
+# The Dreamie Bot starts here.
+bot = commands.Bot(command_prefix=BOT_PREFIX.values(), owner_id=auth_config.OWNER)
+all_extensions = [x.replace('.py', '') for x in os.listdir('cogs') if x.endswith('.py')]
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+# What command we want to provide in this bot:
+# For users they can use these commands:
+#   'request': to request a new villager. The bot should generate a request ID and send back to user.
+#   'status': to check the status of their requests. Status enum(Enumerated type) are:
+#     ['PENDING', 'PROCEED', 'FOUND', 'CLOSED', 'READY', 'CANCEL']
+#     PENDING: The default status of every new request, which is unreviewed and not approved yet.
+#     PROCEED: When a staff has reviewed and approved a request, the status is changed to proceed.
+#     FOUND: A staff has found a requested villager and fostered to wait for an open plot.
+#     CLOSED: The request has been fulfilled and closed.
+#     READY: A user indicates that there is an open plot, will be ready to welcome a dreamie home.
+#     CANCEL: A request is cancelled before completion. It is either cancelled by a user or a staff.
+#   'ready': to indicate the user is ready now or in the next three days. required argument: a request ID.
+#   'cancel': to cancel a request. required argument: a request ID.
+# ========================================================
+# For staff, we can do these:
+#   'list': List all requests or a specified request. Including cancelled and closed requests.
+#   'review': Review a request to approve or deny, so that move it to the next state (ready to proceed & recruit a villager for them)
+#     required argument: a request ID.
+#   'found': A staff has found a villager, and moved into a fostering house. required argument: a request ID.
+#   'close': Close a request. required argument: a request ID.
+
+def load_extension(cog, path='cogs.'):
+    members = inspect.getmembers(cog)
+    for name, member in members:
+        if name.startswith('on_'):
+            bot.add_listener(member, name)
+    try:
+        bot.load_extension(f'{path}{cog}')
+    except Exception as e:
+        print(f'LoadError: {cog}\n{type(e).__name__}: {e}')
+        logger.warning('LoadError: %s', str(e))
+
+
+def load_extensions(cogs, path='cogs.'):
+    for cog in cogs:
+        members = inspect.getmembers(cog)
+        for name, member in members:
+            if name.startswith('on_'):
+                bot.add_listener(member, name)
+        try:
+            bot.load_extension(f'{path}{cog}')
+        except Exception as e:
+            print(f'LoadError: {cog}\n{type(e).__name__}: {e}')
+            logger.warning('LoadError: %s', str(e))
+
+
+load_extensions(all_extensions)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(f"Fostering Dreamies"), afk=True)
+    _uptime = datetime.datetime.utcnow()
+    url = f"https://discordbots.org/api/bots/{bot.user.id}/stats"
+
+    bot._last_result = None
+    bot.session = aiohttp.ClientSession()
+
+    print(f'{bot.user.name} has connected to Discord@{_uptime}!')
+    logger.info('Connected to Discord.')
+
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!') or message.content.startswith('?'):
+        if isinstance(message.channel, discord.DMChannel):
+            await bot.process_commands(message)
+        else:
+            await message.channel.send('Only accept bot commands in DMs.')
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.DisabledCommand):
+        em = utils.get_embed('red', f'_{ctx.command}_ command has been disabled.')
+        await ctx.send(embed=em)
+
+    elif isinstance(error, commands.MissingRequiredArgument):
+        em = utils.get_embed('red', f'_{ctx.command}_ command missed a required argument.')
+        await ctx.send(embed=em)
+
+    elif isinstance(error, commands.BadArgument):
+        em = utils.get_embed('red', f'_{ctx.command}_ command called a bad argument.')
+        await ctx.send(embed=em)
+    else:
+        await ctx.send(error)
+
+
+@bot.command(name='ping', group='Bot Itself')
+async def ping(ctx):
+    '''Pong! Get the bot's response time'''
+    em = utils.get_embed('green', f'{bot.latency * 1000} ms', title='Pong!')
+    await ctx.send(embed=em)
+
+
+@bot.command(name='invite', group='Bot Itself')
+async def invite(ctx):
+    '''Invite the bot to your server'''
+    em = utils.get_embed('gray', f'Invite me to your server: {INVITE_URL}', title='Invite me!')
+    await ctx.send(embed=em)
+
+
+@bot.command(hidden=True)
+@commands.is_owner()
+async def reload(ctx, cog=None):
+    """Reloads a cog"""
+    cog = cog or 'all'
+    is_owner = await ctx.bot.is_owner(ctx.author)
+    if not is_owner:
+        logger.info('User %s wanted to run priviledged command.' % ctx.author)
+        return
+    if cog.lower() == 'all':
+        for cog in all_extensions:
+            try:
+                bot.unload_extension(f"cogs.{cog}")
+            except Exception as e:
+                await ctx.send(f"An error occured while reloading {cog}, error details: \n ```{e}```")
+        load_extensions(all_extensions)
+        return await ctx.send('All cogs updated successfully :white_check_mark:')
+    if cog not in all_extensions:
+        return await ctx.send(f'Cog {cog} does not exist.')
+    try:
+        bot.unload_extension(f"cogs.{cog}")
+        await asyncio.sleep(1)
+        load_extension(cog)
+    except Exception as e:
+        await ctx.send(f"An error occured while reloading {cog}, error details: \n ```{e}```")
+    else:
+        await ctx.send(f"Reloaded the {cog} cog successfully :white_check_mark:")
+
+
+@bot.command(name='shutdown', aliases=['shut'], hidden=True)
+@commands.is_owner()
+async def shutdown(ctx):
+    '''Shut down the bot'''
+    em = utils.get_embed('red', 'Shutting down....', title='Offline!')
+    await ctx.send(embed=em)
+    await bot.logout()
+    logger.info('Disconnected from Discord. Shutting down the bot.')
+
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
---
+++ ext/sheet.py
@@ -0,0 +1,155 @@
+'''Modify applicant data in a Google Sheet by the bot.'''
+import gspread
+from gspread_formatting import *
+
+from ext import utils
+
+# Sheet ID in the url, just behind https://docs.google.com/spreadsheets/d/<ID is here>
+GSHEET_ID='1rrwexbsVgOoKslDllp7-Sc0TPwoJxP9W_yKesg02tpo'
+# Or if the spreadhseet has been shared with you, you can search by its title.
+GSHEET_TITLE='Test discordbot-updater sheets'
+# Which worksheet will the bot modify
+GSHEET_WORKSHEET='Sheet1'
+
+gc = gspread.service_account()
+
+# Open a sheet from a spreadsheet in one go
+sheet = gc.open(GSHEET_TITLE)
+wks = sheet.worksheet(GSHEET_WORKSHEET)
+
+
+def format_color(status, time_travel=False):
+    '''Format different colors by status.'''
+    cell_color = None
+    # Setup dictionaries of cell formats.
+    text_format = dict()
+    tmp = dict()
+    tmp['fontFamily'] = 'Arial'
+    tmp['fontSize'] = 10
+    text_format['textFormat'] = tmp
+
+    red_text_format = text_format.copy()
+    tmp_red = red_text_format['textFormat']
+    tmp_red ['foregroundColor'] = {
+        'red': 1}
+
+    # Status = PENDING, it is also the base of other colors.
+    cell_white = dict()
+    cell_white['horizontalAlignment'] = 'center'
+    cell_white['verticalAlignment'] = 'bottom'
+    cell_white['wrapStrategy'] = 'OVERFLOW_CELL'
+    cell_white['hyperlinkDisplayType'] = 'PLAIN_TEXT'
+    cell_white['backgroundColor'] = {
+        'red': 1,
+        'green': 1,
+        'blue': 1}
+
+    # Status = PROCEED
+    cell_yellow = cell_white.copy()
+    cell_yellow['backgroundColor'] = {
+        'red': 1,
+        'green': 1}
+
+    # Status = FOUND
+    cell_orange = cell_white.copy()
+    cell_orange['backgroundColor'] = {
+        'red': 1,
+        'green': 0.6}
+
+    # Status = CLOSED
+    cell_green = cell_white.copy()
+    cell_green['backgroundColor'] = {
+        'red': 0,
+        'green': 1}
+
+    # Status = READY [user is ready].
+    cell_greenaccent = cell_white.copy()
+    cell_greenaccent['backgroundColor'] = {
+        'red': 0.20392157,
+        'green': 0.65882355,
+        'blue': 0.3254902}
+
+    # Status = CANCEL [user or staff cancelled a request]
+    cell_gray = cell_white.copy()
+    cell_gray['backgroundColor'] = {
+        'red': 0.6,
+        'green': 0.6,
+        'blue': 0.6}
+
+    if str(time_travel).lower() == 'false':
+        red_text_format = red_text_format
+    else:
+        red_text_format = None
+
+    if status == utils.Status.PENDING.name:
+        cell_color = cell_white
+    if status == utils.Status.PROCEED.name:
+        cell_color = cell_yellow
+    if status == utils.Status.FOUND.name:
+        cell_color = cell_orange
+    if status == utils.Status.CLOSED.name:
+        cell_color = cell_green
+    if status == utils.Status.READY.name:
+        cell_color = cell_greenaccent
+    if status == utils.Status.CANCEL.name:
+        cell_color = cell_gray
+    return (cell_color, red_text_format)
+
+
+def update_data(data):
+    '''Update a single row in the spreadsheet and fill changes up with passed data.'''
+    last_modified = ''
+    new_data = []
+    for request_id, details in data.items():
+        new_data.append(str(request_id))
+        new_data.append(details['name'])
+        new_data.append(details['status'])
+        villager = details['villager']
+        # only need the name put in the sheet.
+        new_data.append(villager.split(',')[0])
+        new_data.append(details['created_time'])
+        new_data.append(details['can_time_travel'])
+        new_data.append(details['avail_time'])
+        try:
+            new_data.append(details['last_modified'])
+        except KeyError:
+            # normal case: some requests don't have a last_modified data yet.
+            pass
+
+    try:
+        # Updating an existing data.
+        cell = wks.find(str(request_id))
+        row = cell.row
+        for i in range(1, len(new_data)+1):
+            wks.update_cell(row, i, new_data[i-1])
+        wks.format('A:H', {"horizontalAlignment": "CENTER"})
+        # TODO: add comment/note to status field and record the latest change.
+        (color, red_text) = format_color(details['status'],
+                                 details['can_time_travel'])
+        data_range = 'A{}:H{}'.format(row, row)
+        # Update color and text format.
+        wks.format(data_range, color)
+        if red_text:
+            wks.format(data_range, red_text)
+    except gspread.exceptions.CellNotFound:
+        # Appending new data
+        all_data = wks.get_all_records()
+        if not all_data:
+        # write headers first..
+            wks.append_row(['Request Id', 'Name', 'Status', 'Villager',
+                            'Created Time(UTC)', 'Time-Travel',
+                            'Available Time(UTC)', 'Last Modified(UTC)'])
+            wks.format('A1:H1', {'textFormat': {'bold': True}})
+        wks.append_row(new_data)
+        wks.format('A:H', {"horizontalAlignment": "CENTER"})
+
+        color, red_text = format_color(details['status'],
+                                 details['can_time_travel'])
+        # Headers don't count as data.
+        # And we've insert a new row, so move 2 by the length.
+        row = len(all_data) + 2
+        data_range = 'A{}:H{}'.format(row, row)
+        # Update color and text format.
+        wks.format(data_range, color)
+        if red_text:
+            wks.format(data_range, red_text)
