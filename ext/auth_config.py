4d361ff696e50a0f9ded1af14d952651d6b32774
Foxfair Hu <foxfair@gmail.com>
2020-07-17 06:05:39 +0000

    Semi-release check in. Add new functions for sheet update & archive and others.

--- ext/auth_config.py
+++ ext/auth_config.py
@@ -1,9 +1,21 @@
 # A list contains authorized users' discord IDs.
-OWNER = 184335517947658240      # foxfair
+OWNER = 184335517947658240  # foxfair
 
 # Staff
 AUTHORIZED = [
     OWNER,
-    129405976020385792,  # Thutaloo
+    129405976020385792,  # Auri
     423991805156261889,  # Kim
+    699053180079702098,  # Gelica
+    266289895415218177,  # Yang
+    294058604854509589,  # Giana
+    107209352816914432,  # Tooch
+    97145923691347968,   # baosao
+    137798184721186817,  # Vince
+]
+
+# Channels that the bot is authorized to send messages.
+SEND_MSG_CHANNELS = [
+    725807955559055451,     # bot-logs in my dev server.
+    # 725798620531785749,     # villager-adoption-team in beyond stalks
 ]
