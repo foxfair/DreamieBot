715a0a83bdf37df2e4ff2eec6da0c25761c32a9d
Foxfair Hu <foxfair@gmail.com>
2020-07-17 06:03:20 +0000

    Semi-release check in, lots of wording changes and add background cog for monitoring tasks.
    Use yapf to format code to be more readable.

---
+++ cogs/background.py
@@ -0,0 +1,63 @@
+'''Background tasks module.'''
+
+import discord
+from discord.ext import commands, tasks
+
+from ext import auth_config
+from ext import utils
+
+
+class Background(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.last_status = dict()
+        self.monitoring.start()
+
+    def cog_unload(self):
+        self.monitoring.cancel()
+
+    @tasks.loop(minutes=15.0)
+    async def monitoring(self):
+        '''Monitoring applications and report it back to #adoption-team's channel.'''
+        # For testing, lets just spam foxfair.
+        # user = self.bot.get_user(self.bot.owner_id)
+        # dm_chan = user.dm_channel or await user.create_dm()
+        # Send message to #bot-logs channel.
+        chan = self.bot.get_channel(auth_config.SEND_MSG_CHANNELS[0])
+        staff = self.bot.get_cog('Staff')
+        for status in ('pending', 'found', 'approved', 'ready', 'processing'):
+            report = await staff.search_cmd(status, 'background')
+            title = '_%s_ Application' % status.capitalize()
+            if len(report) > 1:
+                title += 's: *%d*' % len(report)
+            else:
+                title += ': *%d*' % len(report)
+            if report:
+                embed = discord.Embed(title=title)
+                embed.color = utils.random_color()
+                for k, v in report.items():
+                    embed.add_field(name=k, value=v, inline=True)
+                # Save to self.last_status, and only report different/new info.
+                # "if status not in self.last_status" means this is a new status.
+                # "if report != self.last_status[status]" means data of this status
+                # has changed.
+                if status not in self.last_status or report != self.last_status[
+                        status]:
+                    self.last_status[status] = report
+                    # await dm_chan.send(embed=embed)
+                    await chan.send(embed=embed)
+
+    @monitoring.before_loop
+    async def before_monitoring(self):
+        await self.bot.wait_until_ready()
+
+    @tasks.loop(minutes=60.0)
+    async def countdown(self):
+        '''Monitoring user's ready case, and remind them when the time remains 6,3 and 1 hour.'''
+        staff = self.bot.get_cog('Staff')
+        report = await staff.search_cmd('ready', 'background')
+
+
+def setup(bot):
+    bot.add_cog(Background(bot))
+    print('Background module loaded.')
--- cogs/user.py
+++ cogs/user.py
@@ -21,7 +21,6 @@ LOG_CHANNEL = os.getenv('LOG_CHANNEL')
 
 class User(commands.Cog):
     '''User commands to maintain a villager application.'''
-
     def __init__(self, bot):
         self.bot = bot
         self._last_result = None
@@ -34,11 +33,12 @@ class User(commands.Cog):
         channel = self.bot.get_channel(int(LOG_CHANNEL))
         await channel.send(data)
 
-    @commands.command(name='apply', aliases=['app'],
-                      usage='<villager name>, i.e: !apply Raymond')
+    @commands.command(name='apply',
+                      aliases=['app'],
+                      usage='<villager name>, i.e: ~apply Raymond')
     async def apply(self, ctx, villager=None):
         '''Apply to request a villager and get an <application ID> in return.'''
-        
+
         # Setup a Flow controller.
         flow = request.Flow(self.bot, ctx)
         # Find is_bot_locked from Staff cog.
@@ -51,28 +51,43 @@ class User(commands.Cog):
             return await ctx.channel.send(embed=em)
         if not villager:
             text = 'Greetings! Which villager would you like to request?\n'
-            text += 'Use **!apply <villager>** command to create an application.'
+            text += 'Use **~apply <villager>** command to create an application.'
             em = utils.get_embed('gray', text)
             return await ctx.channel.send(embed=em)
-
-        # Form a villager link
-        villager_link = 'https://villagerdb.com/villager/{}'.format(villager.lower())
-        villager = villager.capitalize()
+        # Viallger name differeniation: some villagers have a space char
+        # between its names:
+        v = villager.lower()
+        if v is not 'kidd' and v in ('kid', 'agent', 'big', 'wart'):
+            if v == 'kid':
+                villager = 'Kid Cat'
+                villager_link = 'https://villagerdb.com/villager/kid-cat'
+            if v == 'agent':
+                villager = 'Agent S'
+                villager_link = 'https://villagerdb.com/villager/agent-s'
+            if v == 'big':
+                villager = 'Big Top'
+                villager_link = 'https://villagerdb.com/villager/big-top'
+            if v == 'wart':
+                villager = 'Wart Jr.'
+                villager_link = 'https://villagerdb.com/villager/wart-jr'
+        else:
+            # Form a villager link
+            villager_link = 'https://villagerdb.com/villager/{}'.format(
+                villager.lower())
+            villager = villager.capitalize()
         # Validate the villager's name before everything.
         result = checks.validate_name(villager)
         if result:
-            await ctx.send(result)
-            return
+            return await ctx.send(result)
         villager_data = "{}, {}".format(villager, villager_link)
 
         request_id = ctx.message.id
         name = ctx.message.author.name
-        
+        name += '#' + ctx.message.author.discriminator
         message = checks.precheck(name, villager)
         if message:
             em = utils.get_embed('red', message, title='Precheck Failed')
-            await ctx.channel.send(embed=em)
-            return
+            return await ctx.channel.send(embed=em)
 
         tm = time.localtime(time.time())
         timestring = time.strftime("%Y-%m-%d %I:%M:%S%p %Z", tm)
@@ -89,47 +104,61 @@ class User(commands.Cog):
         details['villager'] = villager_data
         data += u"CreatedTime: {}\n".format(timestring)
         details['created_time'] = timestring
+        # default null for these two.
+        details['last_modified'] = ''
+        details['staff'] = ''
 
         em = utils.get_embed('gray', 'Your Application Details:')
         await ctx.channel.send(embed=em)
         await ctx.channel.send(utils.printadict(details, hide_self=True))
         time.sleep(1)
         # Flow control.
-        tt_or_not = await ctx.send(f":information_source: Are you willing to do Time Travel in order to "
-                                   f"make the process quicker?")
+        tt_or_not = await ctx.send(
+            f":information_source: Are you willing to do Time Travel in order to "
+            f"make the process quicker?")
         tt_reaction = await flow.get_yes_no_reaction_confirm(tt_or_not, 200)
         if tt_reaction is None:
-            return ctx.send("Application cancelled. You may apply again at any time.")
+            return ctx.send(
+                "Application cancelled. You may apply again at any time.")
         if not tt_reaction:
             details['can_time_travel'] = False
             # Only accept non-tter with an open plot in 72 hours to apply.
-            within_72_hr = await ctx.send(f":question: Will you have an open plot with 72 hours? ")
-            reaction = await flow.get_yes_no_reaction_confirm(within_72_hr, 200)
+            within_72_hr = await ctx.send(
+                f":question: Will you have an open plot with 72 hours? ")
+            reaction = await flow.get_yes_no_reaction_confirm(
+                within_72_hr, 200)
             if reaction is None:
-                return ctx.send("Application cancelled. You may apply again at any time.")
+                return ctx.send(
+                    "Application cancelled. You may apply again when an open plot is ready."
+                )
             if not reaction:
-                rejected_text = ("We’re sorry, your application cannot be accepted at this time.\n"
-                                 "Please apply again when you are within a 72 hour window "
-                                 "of an open plot.")
-                em = utils.get_embed('red', rejected_text, title="Application Denied")
+                rejected_text = (
+                    "We’re sorry, your application cannot be accepted at this time.\n"
+                    "Please apply again when you are within a 72 hour window "
+                    "of an open plot.")
+                em = utils.get_embed('red',
+                                     rejected_text,
+                                     title="Application Denied")
                 return await ctx.channel.send(embed=em)
         else:
             details['can_time_travel'] = True
 
         # Available timeslot selection
-        timeslot = await ctx.send(f":information_source: Which time slot is the best choice to contact you"
-                                   " if we have to? \n"
-                                   "NOTE: Please refer to https://time.is/UTC and select your most available"
-                                   " timeslot in **UTC**.\n"
-                                   ":sparkles: Slot 1: 00:00 - 03:59 UTC.\n:eight_spoked_asterisk: "
-                                   "Slot 2: 04:00 - 07:59 UTC.\n"
-                                   ":heart: Slot 3: 08:00 - 11:59 UTC.\n:rocket: Slot 4: 12:00 - 15:59 UTC.\n"
-                                   ":crescent_moon: Slot 5: 16:00 - 19:59 UTC.\n:full_moon: Slot 6: "
-                                   "20:00 - 23:59 UTC.\n")
+        timeslot = await ctx.send(
+            f":information_source: Which time slot is the best choice to contact you"
+            " if we have to? \n"
+            "NOTE: Please refer to https://time.is/UTC and select your most available"
+            " timeslot in **UTC**.\n"
+            ":sparkles: Slot 1: 00:00 - 03:59 UTC.\n:eight_spoked_asterisk: "
+            "Slot 2: 04:00 - 07:59 UTC.\n"
+            ":heart: Slot 3: 08:00 - 11:59 UTC.\n:rocket: Slot 4: 12:00 - 15:59 UTC.\n"
+            ":crescent_moon: Slot 5: 16:00 - 19:59 UTC.\n:full_moon: Slot 6: "
+            "20:00 - 23:59 UTC.\n")
         time.sleep(1)
-        slot_reaction = await flow.get_timeslot_reaction_confirm(timeslot, 200)
+        slot_reaction = await flow.get_timeslot_reaction_confirm(timeslot, 600)
         if slot_reaction is None:
-            return  ctx.send("Application cancelled. You may apply again at any time.")
+            return ctx.send(
+                "Application cancelled. You may apply again at any time.")
         if slot_reaction == 1:
             details['avail_time'] = '00:00-03:59 UTC'
         if slot_reaction == 2:
@@ -144,18 +173,22 @@ class User(commands.Cog):
             details['avail_time'] = '20:00-23:59 UTC'
 
         # Final confirmation
-        are_you_sure = await ctx.send(f":question: Please confirm YES/NO to add a new "
-                                      f"application of finding **%s**" % villager)
+        are_you_sure = await ctx.send(
+            f":question: Please confirm YES/NO to create a new "
+            f"application of finding **%s**" % villager)
 
         reaction = await flow.get_yes_no_reaction_confirm(are_you_sure, 200)
         if reaction is None:
-            return ctx.send("Application cancelled. You may apply again at any time.")
+            return ctx.send(
+                "Application cancelled. You may apply again at any time.")
         if not reaction:
             return await ctx.send("Aborted your application.")
         elif reaction:
             user_obj = self.bot.get_user(ctx.message.author.id)
             await ctx.send("This application has been logged for review.")
-            await ctx.send("%s Please take a note of your application ID:\n**%s**" % (user_obj.mention, request_id))
+            await ctx.send(
+                "%s Please take a note of your application ID:\n**%s**" %
+                (user_obj.mention, request_id))
             time.sleep(1)
             # Add a default status.
             data += u"Status: {}".format(utils.Status.PENDING.name)
@@ -167,8 +200,10 @@ class User(commands.Cog):
             with open(RECORD_FILE, mode="a") as f:
                 json.dump(data_dict, f, indent=None)
                 f.write('\n')
-            await self.send_logs_user('%s requested a dreamie (%s) at %s' % (name, villager.capitalize(), timestring))
             sheet.update_data(data_dict)
+            await self.send_logs_user('%s requested a dreamie (%s) at %s' %
+                                      (name, villager.capitalize(), timestring)
+                                      )
 
     @commands.command(name='status', aliases=['st'])
     async def status(self, ctx):
@@ -180,7 +215,8 @@ class User(commands.Cog):
             for k, v in list(details.items()):
                 if k == u'name' and ctx.message.author.name in v:
                     # also hide requests with status == 'cancel' or 'closed'
-                    if details['status'] not in (utils.Status.CLOSED.name, utils.Status.CANCEL.name):
+                    if details['status'] not in (utils.Status.CLOSED.name,
+                                                 utils.Status.CANCEL.name):
                         found += 'application_id: **%s**\n' % request_id
                         found += utils.printadict(details, hide_self=True)
                         found += '\n'
@@ -210,26 +246,31 @@ class User(commands.Cog):
                 # user can only mark their own request.
                 for k, v in list(details.items()):
                     if k == u'name' and ctx.message.author.name in v:
-                        found = ('%s has marked the application **%s** as ready to '
-                                 'accept the dreamie!' % (ctx.message.author.name, req_id))
+                        found = (
+                            '%s has marked the application **%s** as ready to '
+                            'accept the dreamie!' %
+                            (ctx.message.author.name, req_id))
                         details['status'] = utils.Status.READY.name
                         dreamie = details['villager']
                         dreamie = dreamie.split(',')[0]
                         # mark a last_modified timestring
                         tm = time.localtime(time.time())
-                        timestring = time.strftime("%Y-%m-%d %I:%M:%S%p %Z", tm)
+                        timestring = time.strftime("%Y-%m-%d %I:%M:%S%p %Z",
+                                                   tm)
                         details['last_modified'] = timestring
                         found_data[request_id] = details
                         break
 
         if not found:
-            em = utils.get_embed('red', 'Cannot found your application %s.' % req_id)
+            em = utils.get_embed('red',
+                                 'Cannot found your application %s.' % req_id)
             await ctx.channel.send(embed=em)
             return
 
-        are_you_sure = await ctx.send(f":information_source: Please confirm YES/NO to indicate"
-                                      f" that you have an open plot ready to receive your dreamie.\n"
-                                      f"Application ID:**%s**" % request_id)
+        are_you_sure = await ctx.send(
+            f":information_source: Please confirm YES/NO to indicate"
+            f" that you have an open plot ready to receive your dreamie.\n"
+            f"Application ID:**%s**" % request_id)
         reaction = await flow.get_yes_no_reaction_confirm(are_you_sure, 200)
 
         if reaction is None:
@@ -242,9 +283,10 @@ class User(commands.Cog):
             em = utils.get_embed(color, found)
             await ctx.channel.send(embed=em)
             utils.flush_requestlog(data_dict)
-            await self.send_logs_user('%s is ready to accept a dreamie(%s).' % (ctx.message.author.name, dreamie))
             sheet.update_data(found_data)
-
+            await self.send_logs_user('%s is ready to accept a dreamie (%s).' %
+                                      (ctx.message.author.name, dreamie))
+            # TODO: add staff dm here.
 
     @commands.command(name='cancel', aliases=['can'])
     async def cancel(self, ctx, req_id):
@@ -262,13 +304,16 @@ class User(commands.Cog):
                 for k, v in list(details.items()):
                     if k == u'name' and ctx.message.author.name in v:
                         message = 'You are about to cancel an application **%s**' % req_id
-                        em = utils.get_embed('red', message, title='Cancel An Application')
+                        em = utils.get_embed('red',
+                                             message,
+                                             title='Cancel An Application')
                         await ctx.channel.send(embed=em)
                         found = True
                         details['status'] = utils.Status.CANCEL.name
                         # mark a last_modified timestring
                         tm = time.localtime(time.time())
-                        timestring = time.strftime("%Y-%m-%d %I:%M:%S%p %Z", tm)
+                        timestring = time.strftime("%Y-%m-%d %I:%M:%S%p %Z",
+                                                   tm)
                         details['last_modified'] = timestring
                         found_data[request_id] = details
                         break
@@ -278,8 +323,9 @@ class User(commands.Cog):
             await ctx.channel.send(embed=em)
             return
 
-        are_you_sure = await ctx.send(f":information_source: Please confirm YES/NO to cancel"
-                                      f" this application **%s**" % request_id)
+        are_you_sure = await ctx.send(
+            f":information_source: Please confirm YES/NO to cancel"
+            f" this application **%s**" % request_id)
 
         reaction = await flow.get_yes_no_reaction_confirm(are_you_sure, 200)
         if reaction is None:
@@ -290,7 +336,8 @@ class User(commands.Cog):
         elif reaction:
             utils.flush_requestlog(data_dict)
             await ctx.send(f"You\'ve cancelled this application.")
-            await self.send_logs_user('Cancelled application %s by %s' % (req_id, ctx.message.author.name))
+            await self.send_logs_user('Cancelled application %s by %s' %
+                                      (req_id, ctx.message.author.name))
             sheet.update_data(found_data)
 
 
