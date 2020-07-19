9c5d50271a059b10735ed3681d28ede32b967413
Foxfair Hu <foxfair@gmail.com>
2020-07-19 16:24:50 +0000

    Semi-release weekly: Add a new background cog. It is monitoring application status periodically, only sending DMs out when there is application changing status in the last 15 minutes. Utilitize staff's search command. The countdown() is unfinished as this check-in.

--- cogs/background.py
+++ cogs/background.py
@@ -16,17 +16,17 @@ class Background(commands.Cog):
     def cog_unload(self):
         self.monitoring.cancel()
 
-    @tasks.loop(minutes=15.0)
+    @tasks.loop(minutes=15)
     async def monitoring(self):
         '''Monitoring applications and report it back to #adoption-team's channel.'''
         # For testing, lets just spam foxfair.
-        # user = self.bot.get_user(self.bot.owner_id)
-        # dm_chan = user.dm_channel or await user.create_dm()
+        user = self.bot.get_user(self.bot.owner_id)
+        dm_chan = user.dm_channel or await user.create_dm()
         # Send message to #bot-logs channel.
-        chan = self.bot.get_channel(auth_config.SEND_MSG_CHANNELS[0])
+        # chan = self.bot.get_channel(auth_config.SEND_MSG_CHANNELS[0])
         staff = self.bot.get_cog('Staff')
         for status in ('pending', 'found', 'approved', 'ready', 'processing'):
-            report = await staff.search_cmd(status, 'background')
+            report = await staff.search(None, status, 'background')
             title = '_%s_ Application' % status.capitalize()
             if len(report) > 1:
                 title += 's: *%d*' % len(report)
@@ -44,8 +44,8 @@ class Background(commands.Cog):
                 if status not in self.last_status or report != self.last_status[
                         status]:
                     self.last_status[status] = report
-                    # await dm_chan.send(embed=embed)
-                    await chan.send(embed=embed)
+                    await dm_chan.send(embed=embed)
+                    # await chan.send(embed=embed)
 
     @monitoring.before_loop
     async def before_monitoring(self):
@@ -55,7 +55,7 @@ class Background(commands.Cog):
     async def countdown(self):
         '''Monitoring user's ready case, and remind them when the time remains 6,3 and 1 hour.'''
         staff = self.bot.get_cog('Staff')
-        report = await staff.search_cmd('ready', 'background')
+        report = await staff.search(None, 'ready', 'background')
 
 
 def setup(bot):
